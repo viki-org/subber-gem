b862a637523972173b3cce9bf25babdd6809c1df
Duong Nguyen <ntd251@users.noreply.github.com>
2018-12-11 11:36:21 +0800

    Moved file conversion to base file

--- lib/subber/file/vtt.rb
+++ lib/subber/file/vtt.rb
@@ -2,11 +2,5 @@ module Subber::File
   class Vtt < Base
     parser Subber::Parser::Vtt
     formatter Subber::Formatter::Vtt
-
-    # @return [Subber::File::Srt]
-    #
-    def to_srt
-      Subber::File::Srt.new(subtitles: subtitles)
-    end
   end
 end
